d623bc16fe84fd936e2974b1d2933b7a37bebd44
Your Name <lobozinho.ow.2210@gmail.com>
2023-08-31 12:42:43 +0700

    Doing UI Drag and Drop

--- Assets/_Script/UI/DragAndDrop/ObjectDragAndDrop.cs
+++ Assets/_Script/UI/DragAndDrop/ObjectDragAndDrop.cs
@@ -6,30 +6,38 @@ using UnityEngine.EventSystems;
 public class ObjectDragAndDrop : LoboMonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
 
-    [SerializeField] private RectTransform _rectTransform;
-    [SerializeField] private CanvasGroup _canvasGroup;
+    //[SerializeField] private RectTransform _rectTransform;
+    //[SerializeField] private CanvasGroup _canvasGroup;
 
-    protected override void LoadComponents()
-    {
-        this._rectTransform = GetComponent<RectTransform>();
-        this._canvasGroup = GetComponent<CanvasGroup>();
-    }
+    //protected override void LoadComponents()
+    //{
+    //    this._rectTransform = GetComponent<RectTransform>();
+    //    this._canvasGroup = GetComponent<CanvasGroup>();
+    //}
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        this._canvasGroup.alpha = .6f;
-        this._canvasGroup.blocksRaycasts = false;
+        //this._canvasGroup.alpha = .6f;
+        //this._canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        this._rectTransform.anchoredPosition += eventData.delta;
+        //this._rectTransform.anchoredPosition += eventData.delta;
+        Vector3 mousePos = this.GetMousePos();
+        mousePos.z = 0;
+        transform.position = mousePos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        this._canvasGroup.alpha = 1f;
-        this._canvasGroup.blocksRaycasts = true;
+        //this._canvasGroup.alpha = 1f;
+        //this._canvasGroup.blocksRaycasts = true;
+    }
+
+    Vector3 GetMousePos()
+    {
+        return ManagerCtrl.Instance.InputManager.MouseWorldPos;
     }
 
 }

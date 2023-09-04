7f3ec2fb201cde303a3592b696dc3cee9b09c711
Your Name <lobozinho.ow.2210@gmail.com>
2023-09-04 11:54:28 +0700

    Doing update container

--- Assets/_Script/UI/DragAndDrop/ContainersDragAndDrop.cs
+++ Assets/_Script/UI/DragAndDrop/ContainersDragAndDrop.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public abstract class ContainersDragAndDrop : LoboMonoBehaviour, IDropHandler
 {
     [Header("ContainersDragAndDrop")]
     [SerializeField] protected const string CHICKENS_TAG = "Chickens";
+    [SerializeField] protected Image image;
     [SerializeField] protected bool isStandy;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadImage();
+    }
+
+    void LoadImage()
+    {
+        if (this.image != null) return;
+        this.image = GetComponent<Image>();
+        Debug.LogWarning(transform.name + ": LoadImage", gameObject);
+    }
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -33,6 +48,36 @@ public abstract class ContainersDragAndDrop : LoboMonoBehaviour, IDropHandler
         chickenShooting.SetIsStandy(this.isStandy);
     }
 
-    protected virtual void SetIndexStandy(GameObject dropObj) {}    
+    protected virtual void SetIndexStandy(GameObject dropObj) {}
+
+    //public void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    //this.OnEnabledContainerImage();
+    //    Debug.Log("OnTriggerEnter2D");
+    //    Color imageColor = image.color;
+    //    imageColor.a = 0.5f;
+    //    image.color = imageColor;
+    //}
+
+    //public void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    Debug.Log("OnTriggerExit2D");
+    //    Color imageColor = image.color;
+    //    imageColor.a = 0f;
+    //    image.color = imageColor;
+    //    //this.DisabledContainerImage();
+    //}
+
+    //void OnEnabledContainerImage()
+    //{
+    //    if (transform.childCount > 0) return;
+    //    this._containerImage.enabled = true;
+    //}
+
+    //void DisabledContainerImage()
+    //{
+    //    if (transform.childCount > 0) return;
+    //    this._containerImage.enabled = false;
+    //}
 
 }
